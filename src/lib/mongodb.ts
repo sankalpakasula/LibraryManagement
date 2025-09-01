4ef322026bb4600865f777e6f8deef311611ae68
Tati Dheeraj <tatidheeraj@gmail.com>
2025-09-01 07:57:50 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,7 +2,7 @@ import { MongoClient, Db } from 'mongodb';
 
 const uri = process.env.MONGODB_URI;
 if (!uri) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  throw new Error('Please define the MONGODB_URI environment variable inside .env');
 }
 
 let client: MongoClient;
